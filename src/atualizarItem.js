bfe73ad7f42601799dd8e4eba196a2f8d8998da6
Joao Carlos <joaoclambert@hotmail.com>
2023-02-03 00:56:18 -0300

    Atualizar item

---
+++ src/atualizarItem.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const AWS = require("aws-sdk")
+
+async function handler(event) {
+
+  const {itemStatus} = JSON.parse(event.body);
+
+  const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+  await dynamodb.update({
+    TableName: "ItemTable",
+    Key: { id: event.pathParameters.id },
+    UpdateExpression: 'set itemStatus = :itemStatus',
+    ExpressionAttributeValues: {
+      ':itemStatus': itemStatus
+    },
+    ReturnValues: "ALL_NEW"
+  }).promise()
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ mensagem: 'Atualizado com Sucesso'} ),
+  };
+};
+
+
+module.exports = { handler }
