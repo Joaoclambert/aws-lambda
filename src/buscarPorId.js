52bf02ef5834af34b5a84424cd4164215d6ea56b
Joao Carlos <joaoclambert@hotmail.com>
2023-02-03 00:52:28 -0300

    Buscar por id

---
+++ src/buscarPorId.js
@@ -0,0 +1,31 @@
+"use strict";
+
+const AWS = require("aws-sdk");
+
+async function handler(event) {
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+    try {
+        const itemNoDynamo = await dynamodb.get({
+            TableName: "ItemTable",
+            Key: {
+                id: event.pathParameters.id
+            }
+        }).promise();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(itemNoDynamo.Item),
+        };
+
+    } catch (error) {
+        console.log(error)
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({}),
+    };
+};
+
+module.exports = { handler };
