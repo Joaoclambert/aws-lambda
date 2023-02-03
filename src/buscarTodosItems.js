0960a0d6765acdf73e035ad004205b35d715ef78
Joao Carlos <joaoclambert@hotmail.com>
2023-02-03 00:47:40 -0300

    Buscar todos os items

---
+++ src/buscarTodosItems.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const AWS = require("aws-sdk");
+
+async function handler(event) {
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
+
+    let items;
+
+    try {
+        const listaDeItems = await dynamodb.scan({
+            TableName: "ItemTable"
+        }).promise();
+
+        return {
+            statusCode: 200,
+            body: JSON.stringify(listaDeItems.Items),
+        };
+
+    } catch (error) {
+        console.log(error)
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify([]),
+    };
+};
+
+module.exports = { handler };
